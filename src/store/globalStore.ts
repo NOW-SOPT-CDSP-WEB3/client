c01d4bbf61d8068436dc40721ab1fe074bf03ce2
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-20 02:10:53 +0900

    feat: atom / read write atom 선언

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -1,12 +1,15 @@
 import { atom } from 'jotai';
 
-//atom 예시 선언
-export const countAtom = atom<number>(0);
+//atom 연습: memberIdAtom
+export const memberIdAtom = atom<number>(1);
 
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
+//read write atom 연습 : readWriteMemberIdAtom
+export const readWriteMemberIdAtom = atom(
+  // 읽기 부분
+  (get) => get(memberIdAtom),
 
-export const todoListAtom = atom<Todo[]>([]);
+  // 쓰기 부분
+  (get, set, newValue: number) => {
+    set(memberIdAtom, newValue);
+  },
+);

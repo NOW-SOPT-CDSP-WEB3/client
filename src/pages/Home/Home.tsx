15f726cbcef6a4d2a5fda5d6ec0764196a68d6c7
moonseonghui <msh9980@naver.com>
2024-05-19 13:47:15 +0900

    feat: homepage에 carousel, card, event 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
+import Carousel from '@/pages/Home/_components/Carousel';
 import HomeCardArea from '@/pages/Home/_components/HomeCardArea';
 import { HOME_CARD_DATA } from '@/pages/Home/_constants/homeCardData';
+
+import EventBox from '@/components/event/EventBox';
 
 import IconVector from '@/assets/svg/vector2.svg?react';
 
 function Home() {
   return (
-    <HomeCardAreaBox>
+    <HomePageLayout>
+      <Carousel />
       <HomeCardArea
         cardTitle='Hyundai Originals'
         cards={HOME_CARD_DATA}
@@ -21,9 +25,16 @@ function Home() {
         cardDetail='현대카드의 오리지널리티를 담은 진정한 현대카드'
         hashtags={['프리미엄', '쓸 때마다 할인', '어디서나 포인트적립', '3F 시스템 혜택']}
       />
-    </HomeCardAreaBox>
+      <EventBox isShowPeriod={false} />
+    </HomePageLayout>
   );
 }
+
+const HomePageLayout = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const HomeCardAreaBox = styled.div`
   display: inline-flex;

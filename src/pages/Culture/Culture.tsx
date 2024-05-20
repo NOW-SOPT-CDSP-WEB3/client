01e4fee9d0f47abfc2c493374edf31d3431fe497
yarimu <yhli0320@naver.com>
2024-05-20 17:49:16 +0900

    docs: CULTURE DATA key값 변경

--- src/pages/Culture/Culture.tsx
+++ src/pages/Culture/Culture.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import App from '@/assets/svg/img_dive_app.svg?react';
+
 import CultureCard from './_components/CultureCard';
 import { CULTURE_DATA } from './_constants/cultureData';
 
@@ -19,16 +21,20 @@ function Culture() {
                     key={Info.name}
                     description={Info.description}
                     image={Info.image}
-                    name={Info.name}
+                    artist={Info.artist}
                     summary={Info.summary}
                     period={Info.period}
                   />
-                ))}{' '}
+                ))}
               </InfoWrapper>
             </CardListWrapper>
           </>
         );
       })}
+      <DiveApp>
+        <AppImg />
+        <AppTitle>현대카드 DIVE</AppTitle>
+      </DiveApp>
     </CultureBody>
   );
 }
@@ -42,18 +48,22 @@ const CultureBody = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  gap: 9.2rem;
   /* align-content: flex-start; */
 `;
 const CardListWrapper = styled.div`
   align-items: flex-start;
 `;
 const Name = styled.h1`
-  font-family: ${({ theme }) => theme.HEAD_01};
+  font-family: ${({ theme }) => theme.FONTS.HEAD_01};
   font-size: ${({ theme }) => theme.FONT_SIZE.HEAD_01};
-  color: ${({ theme }) => theme.HD_BLK};
+  color: ${({ theme }) => theme.COLORS.HD_BLK};
+  font-weight: 400;
   font-style: normal;
   line-height: 48px; /* 137.143% */
   text-align: left;
+  margin-bottom: 4rem;
+  margin-top: 4.8rem;
 `;
 
 const InfoWrapper = styled.div`
@@ -65,4 +75,30 @@ const InfoWrapper = styled.div`
   align-content: flex-start;
   gap: 72px 24px;
   flex-wrap: wrap;
+`;
+
+const DiveApp = styled.div`
+  display: flex;
+  width: 1366px;
+  height: 305px;
+  padding: 39px 0px 38px 0px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  background: ${({ theme }) => theme.COLORS.HD_GRAY_04};
+`;
+
+const AppImg = styled(App)`
+  width: 102px;
+  height: 102px;
+  flex-shrink: 0;
+`;
+
+const AppTitle = styled.p`
+  color: ${({ theme }) => theme.COLORS.HD_BLK};
+  text-align: center;
+  font-family: ${({ theme }) => theme.FONTS.BOLD};
+  font-size: ${({ theme }) => theme.FONT_SIZE.HEAD_04};
+  font-style: normal;
+  line-height: normal;
 `;

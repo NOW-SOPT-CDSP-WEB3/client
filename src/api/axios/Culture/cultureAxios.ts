3ac4b4c8be1781b116467eddcfaca5b793b6857c
yarimu <yhli0320@naver.com>
2024-05-21 18:25:00 +0900

    feat: culture page의 api구현(mock data 삭제)

---
+++ src/api/axios/Culture/cultureAxios.ts
@@ -0,0 +1,15 @@
+import { AxiosError } from 'axios';
+
+import instance from '../instance';
+
+export const getCultureData = async () => {
+  try {
+    const response = await instance.get(`/api/cultures`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      const response = error.response;
+      return response;
+    }
+  }
+};

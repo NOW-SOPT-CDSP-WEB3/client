c2aece5d26f0d01f8c679f7ef7da487caff35a40
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-23 01:33:45 +0900

    remove: 안쓰는 jotai globalStore 삭제

--- src/store/globalStore.ts
+++ src/store/globalStore.ts
@@ -2,17 +2,3 @@ import { atom } from 'jotai';
 
 //atom 연습: memberIdAtom
 export const memberIdAtom = atom<number>(1);
-
-// readonly atom 연습
-export const readMemberIdAtom = atom((get) => get(memberIdAtom));
-
-//read write atom 연습 : readWriteMemberIdAtom
-export const readWriteMemberIdAtom = atom(
-  // 읽기 부분
-  (get) => get(memberIdAtom),
-
-  // 쓰기 부분
-  (get, set, newValue: number) => {
-    set(memberIdAtom, newValue);
-  },
-);

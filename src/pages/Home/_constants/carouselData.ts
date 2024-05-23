f574fd1bf6b4658c76c0bd1350a3c3800deee737
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-24 02:43:26 +0900

    fix: 이미지 경로 수정

--- src/pages/Home/_constants/carouselData.ts
+++ src/pages/Home/_constants/carouselData.ts
@@ -1,20 +1,27 @@
+import scrollUrl1 from '@/assets/images/home_img_scroll_01.png';
+import scrollUrl2 from '@/assets/images/home_img_scroll_02.png';
+import scrollUrl3 from '@/assets/images/home_img_scroll_03.png';
+import scrollUrl4 from '@/assets/images/home_img_scroll_04.png';
+import scrollUrl5 from '@/assets/images/home_img_scroll_05.png';
+import scrollUrl6 from '@/assets/images/home_img_scroll_06.png';
+
 export const CAROUSEL_DATA = [
-  { src: 'src/assets/images/home_img_scroll_01.png', alt: 'image1' },
-  { src: 'src/assets/images/home_img_scroll_02.png', alt: 'image2' },
-  { src: 'src/assets/images/home_img_scroll_03.png', alt: 'image3' },
-  { src: 'src/assets/images/home_img_scroll_04.png', alt: 'image4' },
-  { src: 'src/assets/images/home_img_scroll_05.png', alt: 'image5' },
-  { src: 'src/assets/images/home_img_scroll_06.png', alt: 'image6' },
-  { src: 'src/assets/images/home_img_scroll_01.png', alt: 'image7' },
-  { src: 'src/assets/images/home_img_scroll_02.png', alt: 'image8' },
-  { src: 'src/assets/images/home_img_scroll_03.png', alt: 'image9' },
-  { src: 'src/assets/images/home_img_scroll_04.png', alt: 'image10' },
-  { src: 'src/assets/images/home_img_scroll_05.png', alt: 'image11' },
-  { src: 'src/assets/images/home_img_scroll_06.png', alt: 'image12' },
-  { src: 'src/assets/images/home_img_scroll_01.png', alt: 'image13' },
-  { src: 'src/assets/images/home_img_scroll_02.png', alt: 'image14' },
-  { src: 'src/assets/images/home_img_scroll_03.png', alt: 'image15' },
-  { src: 'src/assets/images/home_img_scroll_04.png', alt: 'image16' },
-  { src: 'src/assets/images/home_img_scroll_05.png', alt: 'image17' },
-  { src: 'src/assets/images/home_img_scroll_06.png', alt: 'image18' },
+  { src: scrollUrl1, alt: 'image1' },
+  { src: scrollUrl2, alt: 'image2' },
+  { src: scrollUrl3, alt: 'image3' },
+  { src: scrollUrl4, alt: 'image4' },
+  { src: scrollUrl5, alt: 'image5' },
+  { src: scrollUrl6, alt: 'image6' },
+  { src: scrollUrl1, alt: 'image7' },
+  { src: scrollUrl2, alt: 'image8' },
+  { src: scrollUrl3, alt: 'image9' },
+  { src: scrollUrl4, alt: 'image10' },
+  { src: scrollUrl5, alt: 'image11' },
+  { src: scrollUrl6, alt: 'image12' },
+  { src: scrollUrl1, alt: 'image13' },
+  { src: scrollUrl2, alt: 'image14' },
+  { src: scrollUrl3, alt: 'image15' },
+  { src: scrollUrl4, alt: 'image16' },
+  { src: scrollUrl5, alt: 'image17' },
+  { src: scrollUrl6, alt: 'image18' },
 ];
--- src/pages/Home/_constants/homeCardData.ts
+++ src/pages/Home/_constants/homeCardData.ts
@@ -1,34 +1,36 @@
+import cardUrl from '@/assets/images/img_card_red.png';
+
 export const HOME_CARD_DATA = [
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5 두줄',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
   {
-    imgSrc: 'src/assets/images/img_card_red.png',
+    imgSrc: cardUrl,
     title: 'the Red Edition5',
   },
 ];
